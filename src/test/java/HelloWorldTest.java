ca21ec29718a650a40ed2da71e9c396dfa0bd8ee
SoSp17 <s.spies@oth-aw.de>
2021-10-20 10:33:33 +0200

    letzte

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,13 +1,10 @@
 import static org.junit.Assert.*;
 
-import org.junit.After;
+
 import org.junit.Test;
 
 public class HelloWorldTest {
 
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void test() {
